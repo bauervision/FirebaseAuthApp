336f6b864a09c763409bb6ad1ebcf26b354dce50
Mike Bauer <mikecbauervision@gmail.com>
2020-10-01 17:10:05 -0400

    wip

--- Assets/Scripts/FirebaseInit.cs
+++ Assets/Scripts/FirebaseInit.cs
@@ -1,18 +1,36 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
+using Firebase.Extensions;
+using System.Threading.Tasks;
 public class FirebaseInit : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
+    [SerializeField] private GameObject initScreen;
+    [SerializeField] private Text initText;
+    protected Firebase.Auth.FirebaseAuth auth;
+    Firebase.DependencyStatus dependencyStatus = Firebase.DependencyStatus.UnavailableOther;
 
+    public bool isAuthenticated;
+
+    // When the app starts, check to make sure that we have
+    // the required dependencies to use Firebase, and if not,
+    // add them if possible.
+    public virtual void Start()
+    {
+        Firebase.FirebaseApp.CheckAndFixDependenciesAsync().ContinueWithOnMainThread(task =>
+        {
+            dependencyStatus = task.Result;
+            if (dependencyStatus == Firebase.DependencyStatus.Available)
+            {
+                isAuthenticated = true;
+                initScreen.SetActive(false);
+            }
+            else
+            {
+                initText.text = "Could not resolve all Firebase dependencies: " + dependencyStatus;
+            }
+        });
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
 }
